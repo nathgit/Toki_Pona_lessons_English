73ec5419e3098f93805549a205bc89ea7c14f2aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-07-05 09:00:19 +0000

    Deploy jan-Lope/Toki_Pona_lessons_English to github.com/jan-Lope/Toki_Pona_lessons_English.git:gh-pages

---
+++ toki-pona-lessons_en/internals.pl
@@ -0,0 +1,154 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/_pi_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:rowa:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:rowa:02/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:olukin:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_numbers_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:astrodonunt:02/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_adjectives_compund_nouns_adverbs_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_questions_using_seme_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_multiple_e_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:Mata:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_unofficial_words_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_conjunctions_temperature_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_la_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_negation_yes_no_questions_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:memrise:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:PaulGoguen:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:astrodonunt:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:Mata:02/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:sowelitoki:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_direct_objects_compund_sentences_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_colors_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_mistakes_with_pi_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:tokipona.org/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:tokipona.net:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_gender_unofficial_words_etc_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_dict_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_pronunciation_alphabet_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_prepositions01_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_other_prepositions_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_multiple_li_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:failbluedot:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_the_body_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_living_things_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:Pije:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_thematic_vocabulary_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_basic_sentences_/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:janKluisa:01/;
+$ref_files{$key} = "$dir".q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ toki-pona-lessons_en/labels.pl
@@ -0,0 +1,253 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/_numbers_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:olukin:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:rowa:02/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:rowa:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_pi_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_questions_using_seme_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:astrodonunt:02/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_adjectives_compund_nouns_adverbs_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:Mata:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_multiple_e_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_la_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_conjunctions_temperature_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_unofficial_words_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:Mata:02/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_negation_yes_no_questions_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:memrise:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:PaulGoguen:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:astrodonunt:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_colors_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:tokipona.org/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_mistakes_with_pi_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:sowelitoki:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_direct_objects_compund_sentences_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_multiple_li_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_other_prepositions_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:tokipona.net:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_dict_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_gender_unofficial_words_etc_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_pronunciation_alphabet_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_prepositions01_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_basic_sentences_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:janKluisa:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_thematic_vocabulary_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:Pije:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_living_things_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_www:failbluedot:01/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_the_body_/;
+$external_labels{$key} = "$URL/" . q|toki-pona-lessons_en.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/_numbers_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_pi_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_negation_yes_no_questions_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_mistakes_with_pi_/;
+$external_latex_labels{$key} = q|1.11|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_colors_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_questions_using_seme_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_adjectives_compund_nouns_adverbs_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_direct_objects_compund_sentences_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_multiple_li_/;
+$external_latex_labels{$key} = q|1.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_other_prepositions_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_prepositions01_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_multiple_e_/;
+$external_latex_labels{$key} = q|1.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_gender_unofficial_words_etc_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_pronunciation_alphabet_/;
+$external_latex_labels{$key} = q|1.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_dict_/;
+$external_latex_labels{$key} = q|A.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_basic_sentences_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_thematic_vocabulary_/;
+$external_latex_labels{$key} = q|A.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_living_things_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_conjunctions_temperature_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_unofficial_words_/;
+$external_latex_labels{$key} = q|A.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_la_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_the_body_/;
+$external_latex_labels{$key} = q|1.19|; 
+$noresave{$key} = "$nosave";
+
+1;
+
